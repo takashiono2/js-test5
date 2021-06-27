0cb2bca2f08b33b8a516295bbd6e8f7ea9aec7a1
takashiono2 <takashiono710@gmail.com>
2021-06-28 00:35:32 +0900

    メニューにイベントを設定しよう

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 'use strict'
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+  menuItems.forEach(item =>{
+    item.addEventListener('click',e =>{
+      e.preventDefault();
+      item.classList.add('active');
+    });
+  });
+}
